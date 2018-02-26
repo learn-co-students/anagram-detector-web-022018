6ab3ea1ca5055c9f97238af95e20e6d25a238917
dannyb21892 <dannyb21892@verizon.net>
2018-02-26 16:06:45 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :input
+
+  def initialize(input)
+    @input = input
+  end
+
+  def match(testwords)
+    testwords.select do |testword|
+      testword.downcase.chars.sort.join == input.downcase.chars.sort.join
+    end
+  end
+
+end# Your code goes here!
