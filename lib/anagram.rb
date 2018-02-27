77906afff79920be540575950dfd9c8b509d9eb9
nkalkstein <nkalkstein@gmail.com>
2018-02-27 17:40:45 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
-# Your code goes here!
+require 'pry'
+
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(anagrams)
+    array = []
+
+      anagrams.each {|word1|
+        if (word1.split("")).sort == (word.split("")).sort
+          array << word1
+        end
+      }
+        array
+  end
+
+    # array = anagrams.map {|word| word.split("")
+    # sorted_array = array.map { |split_word| split_word.sort}
+    # sorted_array.each {|letters|
+    # if word.sort == letters
+    #   return letters
+    # end}
+
+
+end
