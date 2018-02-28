137a4603bac0657e0d9ac78d8360517de8d0f1b1
Barney Fabian <bafa@tutanota.de>
2018-02-28 16:59:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,14 @@
 class Anagram
   attr_accessor :word
-
+  
   def initialize(word)
     @word = word
   end
-
+  
   def match(array)
-    new = []
-    array.each do |x| new << x if (x.split("")).sort == (word.split("")).sort end
+    new = Array.new
+    array.each do |x| new << x if x.split("").sort == word.split("").sort end
     new
   end
+  
 end
